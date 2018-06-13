ddd7876567a557b34d7e2d94422e5e650e3cc9af
David Eder <david.eder@me.com>
2018-06-13 20:08:33 +0200

    list CKAN

---
+++ src/main/java/com/controller/Main.java
@@ -0,0 +1,11 @@
+package com.controller;
+
+public class Main {
+    public static void main(String[] args){
+        Api api = new Api();
+        //api.listDspace();
+        api.listCkan();
+    }
+}
+
+
