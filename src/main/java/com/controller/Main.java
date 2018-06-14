2bb0ec735fd60056248c319d29c98b936d545e0f
David Eder <david.eder@me.com>
2018-06-14 17:54:33 +0200

    main commands

--- src/main/java/com/controller/Main.java
+++ src/main/java/com/controller/Main.java
@@ -3,8 +3,28 @@ package com.controller;
 public class Main {
     public static void main(String[] args){
         Api api = new Api();
-        //api.listDspace();
-        api.listCkan();
+
+
+        if(args.length != 1) {
+            System.out.println("Usage: listDspace | listCkan | migrate");
+            return;
+        } else {
+            if(args[0].equals("listDspace")) {
+                System.out.println("DSpace:");
+                api.listDspace();
+            } else if(args[0].equals("listCkan")) {
+                System.out.println("CKAN:");
+                api.listCkan();
+            } else if(args[0].equals("migrate")) {
+                System.out.println("migrating...");
+                api.migrate();
+            }  else {
+                System.out.println("Usage: listDspace | listCkan | migrate");
+                return;
+            }
+        }
+
+
     }
 }
 
